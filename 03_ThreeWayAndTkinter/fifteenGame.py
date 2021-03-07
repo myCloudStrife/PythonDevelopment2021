b0c53d79f09ec903c5b44b47bc24156874e1f9a5
alex <alextmail@yandex.ru>
2021-03-07 13:28:25 +0300

    fifteenGame: created initial layout
    Same layout as in the task, with window resizing support.

---
+++ 03_ThreeWayAndTkinter/fifteenGame.py
@@ -0,0 +1,63 @@
+import tkinter as tk
+
+
+class FifteenGame(tk.Tk):
+  def __init__(self):
+    super().__init__()
+    self.title("Game of 15")
+    self.columnconfigure(0, weight=1)
+
+    self.rowconfigure(0, weight=0)
+    self.createMenuFrame()
+
+    self.rowconfigure(1, weight=1)
+    self.createPlayFrame()
+
+    self.newGame()
+
+  def createMenuFrame(self):
+    self.menuFrame = tk.Frame(self)
+    self.menuFrame.grid(row=0, column=0, sticky="EW")
+
+    self.newButton = tk.Button(self.menuFrame, text="New", command=self.newGame)
+    self.newButton.grid(row=0, column=0)
+    self.menuFrame.columnconfigure(0, weight=1)
+
+    self.exitButton = tk.Button(self.menuFrame, text="Exit", command=self.quit)
+    self.exitButton.grid(row=0, column=1)
+    self.menuFrame.columnconfigure(1, weight=1)
+
+  def createPlayFrame(self):
+    self.playFrame = tk.Frame(self)
+    self.playFrame.grid(row=1, column=0, sticky="NESW")
+
+    rowsNumber = columnsNumber = 4
+    self.columnsNumber = columnsNumber
+
+    self.playButtons = []
+    for i in range(1, rowsNumber * columnsNumber):
+      playButton = tk.Button(self.playFrame, text=str(i))
+      playButton["command"] = lambda: self.processClick(playButton)
+      self.playButtons.append(playButton)
+
+    for i in range(rowsNumber):
+      self.playFrame.rowconfigure(i, weight=1)
+
+    for i in range(columnsNumber):
+      self.playFrame.columnconfigure(i, weight=1)
+
+
+  def processClick(self, button):
+    pass
+
+  def newGame(self):
+    pos = [0, 0]
+    for button in self.playButtons:
+      button.grid(row=pos[0], column=pos[1], sticky="NESW")
+      pos[1] += 1
+      if (pos[1] == self.columnsNumber):
+        pos[0] += 1
+        pos[1] = 0
+
+
+FifteenGame().mainloop()
